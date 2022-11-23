9de2b4823b18cbea68bb42b01e7996086517ae22
AugustoMagalhaes <augustommg@gmail.com>
2022-11-23 16:00:49 -0300

    fix: altera credenciais corretas pra adequar as regras de disabled do login-btn.

--- src/clockwallet/db.cljs
+++ src/clockwallet/db.cljs
@@ -1,4 +1,4 @@
 (ns clockwallet.db)
 
 (def default-db
-  {:email "" :password "" :credenciais-corretas {:email "admin" :password "admin"}})
+  {:email "" :password "" :credenciais-corretas {:email "admin@admin.com" :password "admin123"}})
