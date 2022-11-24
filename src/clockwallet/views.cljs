d7a19d70529d6f5c415ce8f0a9d93a991004a460
AugustoMagalhaes <augustommg@gmail.com>
2022-11-24 12:37:26 -0300

    feat: atualiza subscribe e dispatch em views.

--- src/clockwallet/views.cljs
+++ src/clockwallet/views.cljs
@@ -4,20 +4,19 @@
             [clockwallet.subs :as subs]
             [re-frame.core :as rf]))
 ;; home
-
 (defn home-panel
   []
-  (let [email (rf/subscribe [::subs/email])
-        senha (rf/subscribe [::subs/senha])
+  (let [email (rf/subscribe [::subs/credenciais-usuario :email])
+        senha (rf/subscribe [::subs/credenciais-usuario :password])
         btn-login-disabled? (rf/subscribe [::subs/disabled-login-btn])]
     [:section.container-c.login
      [:h2.home-title "ClockWallet"]
      [:img.clock-img {:src "/images/clockwal-removebg-preview.png"}]
      [:section.container-c.inputs-login
       [:label.container-c.login-label "Email: "
-       [:input.email {:type "text" :value @email :on-change #(rf/dispatch-sync [::events/update-email (-> % .-target .-value)])}]]
+       [:input.email {:type "text" :value @email :on-change #(rf/dispatch [::events/atualiza-campo :email (-> % .-target .-value)])}]]
       [:label.container-c.login-label "Senha: "
-       [:input.password {:type "password" :value @senha :on-change #(rf/dispatch [::events/update-senha (-> % .-target .-value)])}]]]
+       [:input.password {:type "password" :value @senha :on-change #(rf/dispatch [::events/atualiza-campo :password (-> % .-target .-value)])}]]]
      [:button.login-btn {:disabled @btn-login-disabled? :on-click #(rf/dispatch [::events/try-login])} "Login"]]))
 
 (defmethod routes/panels :home-panel [] [home-panel])
